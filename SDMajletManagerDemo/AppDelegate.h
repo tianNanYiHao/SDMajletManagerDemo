d3077ceca11280a90327bf12317d1972858e5f50
liuff <liu.ff_zf@sand.com.cn>
2017-05-26 15:54:33 +0800

    Initial Commit

---
+++ SDMajletManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDMajletManagerDemo
+//
+//  Created by tianNanYiHao on 2017/5/26.
+//  Copyright © 2017年 tianNanYiHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDMajletManagerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SDMajletManagerDemo
+//
+//  Created by tianNanYiHao on 2017/5/26.
+//  Copyright © 2017年 tianNanYiHao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
